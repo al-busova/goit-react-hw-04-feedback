21e7d60f6e3ae2ce7104c5abcee272347543250e
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-10-07 15:42:15 +0300

    Update FeedBack.jsx

--- src/components/FeedBack/FeedBack.jsx
+++ src/components/FeedBack/FeedBack.jsx
@@ -15,14 +15,14 @@ export const FeedBack = () => {
    
     switch (propertyName) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
         return;
